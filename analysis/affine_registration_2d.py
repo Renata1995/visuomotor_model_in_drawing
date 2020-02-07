e6ebc98d28638d20c371305d882ca087b4491ca2
Renata1995 <renatachai95@gmail.com>
2020-02-07 00:21:26 -0800

    add analysis

---
+++ analysis/affine_registration_2d.py
@@ -0,0 +1,137 @@
+# Copyright 2018 University of Basel, Center for medical Image Analysis and Navigation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import SimpleITK as sitk
+import torch as th
+
+import matplotlib.pyplot as plt
+import time
+import numpy as np
+import cv2
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import airlab as al
+
+
+def affine_reg(img_draw, img_ref, output_path, lr=0.01, iter=200):
+    # set the used data type
+    dtype = th.float32
+    # set the device for the computaion to CPU
+    device = th.device("cpu")
+
+    # In order to use a GPU uncomment the following line. The number is the device index of the used GPU
+    # Here, the GPU with the index 0 is used.
+    # device = th.device("cuda:0")
+
+    # load the image data and normalize to [0, 1]
+    itkImg = sitk.ReadImage(img_ref, sitk.sitkFloat32)
+    itkImg = sitk.RescaleIntensity(itkImg, 0, 1)
+    fixed_image = al.create_tensor_image_from_itk_image(itkImg, dtype=dtype, device=device)
+    fsize = fixed_image.numpy().flatten().size
+
+    itkImg = sitk.ReadImage(img_draw, sitk.sitkFloat32)
+    itkImg = sitk.RescaleIntensity(itkImg, 0, 1)
+    moving_image = al.create_tensor_image_from_itk_image(itkImg, dtype=dtype, device=device)
+
+
+    # create pairwise registration object
+    registration = al.PairwiseRegistration(dtype=dtype, device=device)
+
+    # choose the affine transformation model
+    transformation = al.transformation.pairwise.RigidTransformation(moving_image.size, dtype=dtype, device=device)
+    registration.set_transformation(transformation)
+
+    # choose the scaling regulariser and the diffusion regulariser
+    # scale_reg = al.regulariser.parameter.ScalingRegulariser('trans_parameters')
+    # scale_reg.set_weight(0.0001)
+    # registration.set_regulariser_parameter([scale_reg])
+    # dis_reg = al.regulariser.displacement.DiffusionRegulariser(moving_image.spacing, size_average=False)
+    # registration.set_regulariser_displacement([dis_reg])
+    # dis_reg.set_weight(0.1)
+
+
+    # choose the Mean Squared Error as image loss
+    image_loss = al.loss.pairwise.NCC(fixed_image, moving_image)
+    #init_loss = np.sum(np.square(fixed_image.numpy() - moving_image.numpy()))/fsize
+    registration.set_image_loss([image_loss])
+
+    # choose the Adam optimizer to minimize the objective
+    optimizer = th.optim.Adam(transformation.parameters(), lr=lr)
+
+    registration.set_optimizer(optimizer)
+    registration.set_number_of_iterations(iter)
+
+    # start the registration
+    registration.start()
+
+    # warp the moving image with the final transformation result
+    displacement = transformation.get_displacement()
+    warped_image = al.transformation.utils.warp_image(moving_image, displacement)
+
+    param = transformation.trans_parameters.detach().numpy()
+    translate = np.sqrt(np.square(param[1]) + np.square(param[2]))
+    scale = np.sqrt( ( np.square(param[3]-1) + np.square(param[4] - 1) ) * 0.5  )
+    init_loss = registration.init_loss.detach().numpy()
+    final_loss = registration.img_loss.detach().numpy()
+
+
+    # plot the results
+    # plt.subplot(131)
+    # plt.imshow(fixed_image.numpy(), cmap='gray')
+    # plt.title('Ref')
+
+    plt.figure(figsize=(13, 6))
+    plt.subplot(121)
+    plt.xticks([])
+    plt.yticks([])
+    fixed = fixed_image.numpy()
+    moved = moving_image.numpy()
+    warp = warped_image.numpy()
+    csfont = {'fontname': 'Times New Roman', 'size':35}
+
+    p1 = np.ones((fixed.shape[0], fixed.shape[1], 3))
+    p1[fixed < 1] = [0.5, 0.5, 0.5]
+    p1[moved < 1] = [0.4, 0.62, 0.78]
+    plt.imshow(p1)
+    plt.title('Raw', **csfont)
+
+    plt.subplot(122)
+    plt.xticks([])
+    plt.yticks([])
+    p2 = np.ones((fixed.shape[0], fixed.shape[1], 3))
+    p2[fixed < 1] = [0.5, 0.5, 0.5]
+    p2[warp < 0.5] = [0.4, 0.62, 0.78]
+    plt.imshow(p2)
+    plt.title('Transformed', **csfont)
+
+    plt.savefig(output_path, bbox_inches = 'tight', pad_inches = 0)
+    plt.close()
+
+    return init_loss, final_loss, np.abs(param[0]), translate, scale, warped_image
+
+# img_draw = 'a3.png'
+# img_ref = 'tracing_ref_400/square_ref.png'
+# init_loss, final_loss, ro, tran, scale, warped = affine_reg(img_draw, img_ref, 'transformed.png')
+# print init_loss, final_loss
+
+
+
+
+# write result images
+#sitk.WriteImage(warped_image.itk(), '/tmp/rigid_warped_image.vtk')
+#sitk.WriteImage(moving_image.itk(), '/tmp/rigid_moving_image.vtk')
+#sitk.WriteImage(fixed_image.itk(), '/tmp/rigid_fixed_image.vtk')
---
+++ analysis/svg_render_helpers.py
@@ -0,0 +1,155 @@
+from __future__ import division
+import os
+import urllib, cStringIO
+import pymongo as pm ## first establish ssh tunnel to server where database is running
+import base64
+import numpy as np
+from numpy import *
+import PIL
+from PIL import Image
+import base64
+import matplotlib
+from matplotlib import pylab, mlab, pyplot
+from IPython.core.pylabtools import figsize, getfigs
+plt = pyplot
+import seaborn as sns
+sns.set_context('poster')
+sns.set_style('white')
+from matplotlib.path import Path
+import matplotlib.patches as patches
+
+import pandas as pd
+from svgpathtools import parse_path
+
+
+def get_verts_and_codes(svg_list):
+    '''
+    parse into x,y coordinates and output list of lists of coordinates
+
+    '''
+    lines = []
+    Verts = []
+    Codes = []
+    for stroke_ind, stroke in enumerate(svg_list):
+        x = []
+        y = []
+        parsed = parse_path(stroke)
+        for i, p in enumerate(parsed):
+            if i != len(parsed) - 1:  # last line segment
+                x.append(p.start.real)
+                y.append(p.start.imag)
+            else:
+                x.append(p.start.real)
+                y.append(p.start.imag)
+                x.append(p.end.real)
+                y.append(p.end.imag)
+        lines.append(zip(x, y))
+        verts, codes = polyline_pathmaker(lines)
+        Verts.append(verts)
+        Codes.append(codes)
+    return Verts, Codes
+
+
+def make_svg_list(stroke_recs):
+    '''
+    grab sample drawing's strokes and make a list of svg strings from it
+    '''
+    svg_list = []
+    for strec in stroke_recs:   
+        svg_string = strec['svg']       
+        svg_list.append(svg_string)
+    return svg_list
+
+
+def render_and_save(Verts,
+                    Codes,
+                    outpath,
+                    line_width=5,
+                    imsize=3,
+                    canvas_size=809):
+    '''
+    input:
+        line_width: how wide of strokes do we want? (int)
+        imsize: how big of a picture do we want? (setting the size of the figure)
+        canvas_size: original canvas size on tablet?
+        out_path: where do you want to save your images? currently hardcoded below.
+    output:
+        rendered sketches into nested directories
+
+    '''
+    ## where do you want to save your cumulative drawings?
+
+    verts = Verts[0]
+    codes = Codes[0]
+    fig = plt.figure(frameon=False)
+    dpi = fig.get_dpi()
+    fig.set_size_inches(imsize/dpi, imsize/dpi)
+    ax = plt.subplot(111)
+    ax.axis('off')
+    ax.set_xlim(0, canvas_size)
+    ax.set_ylim(0, canvas_size)
+
+    # remove padding for xaxis and y axis
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+    # remove further paddings
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0,
+                        hspace=0, wspace=0)
+    
+    for i, verts in enumerate(Verts):
+        codes = Codes[i]
+        
+        ### render sketch so far
+        if len(verts) > 0:
+            path = Path(verts, codes)
+            patch = patches.PathPatch(path, facecolor='none', lw=line_width)
+            ax.add_patch(patch)
+            plt.gca().invert_yaxis()  # y values increase as you go down in image
+
+
+    ## save out as png
+    fig.savefig(outpath, bbox_inches='tight', pad_inches=0.0)
+    plt.close(fig)
+
+
+def polyline_pathmaker(lines):
+    x = []
+    y = []
+
+    codes = [Path.MOVETO]  # start with moveto command always
+    for i, l in enumerate(lines):
+        for _i, _l in enumerate(l):
+            x.append(_l[0])
+            y.append(_l[1])
+            if _i < len(l) - 1:
+                codes.append(Path.LINETO)  # keep pen on page
+            else:
+                if i != len(lines) - 1:  # final vertex
+                    codes.append(Path.MOVETO)
+    verts = zip(x, y)
+    return verts, codes
+
+
+def path_renderer(verts, codes):
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(111)
+    if len(verts) > 0:
+        path = Path(verts, codes)
+        patch = patches.PathPatch(path, facecolor='none', lw=2)
+        ax.add_patch(patch)
+        ax.set_xlim(0, 1638)
+        ax.set_ylim(0, 1638)
+        ax.axis('off')
+        plt.gca().invert_yaxis()  # y values increase as you go down in image
+        plt.show()
+    else:
+        ax.set_xlim(0, 1638)
+        ax.set_ylim(0, 1638)
+        ax.axis('off')
+        plt.show()
+    plt.savefig()
+    plt.close()
+
+
+def flatten(x):
+    return [val for sublist in x for val in sublist]
